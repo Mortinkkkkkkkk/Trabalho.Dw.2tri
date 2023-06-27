3670bd6a327aaa755a508950e30f0dedd4845cd3
Fernando J. da Costa Filho <kolumbia3000@gmail.com>
2023-06-27 10:39:57 -0300

    Footer

--- site/viagens.php
+++ site/viagens.php
@@ -4,9 +4,23 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/viagens.css">
     <title>Document</title>
 </head>
 <body>
-    
+    <?php
+        require_once 'conexao.php';
+        $idcliente = $_GET['idcliente'];
+        echo $idcliente;     
+    ?>
+    <div class="card">
+        <div class="imag">
+            <img src="imagens/logo.png" alt="teste">
+        </div>
+        <div class="card-title"><h2>Teste</h2></div>
+        <div class="data">
+            <p>ida - volta</p>
+        </div>
+    </div>
 </body>
 </html>
